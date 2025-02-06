98194b739ed6160863e27fd2b296bc34027e6c1a
Arden Wang <ardentwang@gmail.com>
2025-02-06 15:20:15 -0600

    added pixel text font, adjusted title, added InputOTP with no integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp"
+
+
 export default function Home() {
   return (
-    <div>
-      Hello World
+    <div className="bg-background flex flex-col justify-center items-center min-h-screen col-span-full">
+      <div className="text-7xl mb-5">
+        Badger Bash
+      </div>
+      <div>
+        <InputOTP maxLength={6}>
+          <InputOTPGroup>
+            <InputOTPSlot index={0} />
+            <InputOTPSlot index={1} />
+            <InputOTPSlot index={2} />
+          </InputOTPGroup>
+          <InputOTPSeparator />
+          <InputOTPGroup>
+            <InputOTPSlot index={3} />
+            <InputOTPSlot index={4} />
+            <InputOTPSlot index={5} />
+          </InputOTPGroup>
+        </InputOTP>
+      </div>
     </div>
   );
 }
