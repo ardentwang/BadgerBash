24b020be37c51d8e36eca7d4a04fa579d99b6bf9
Arden Wang <ardentwang@gmail.com>
2025-02-07 10:54:52 -0600

    deleted unused fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,16 +8,6 @@ const pixelifySans = Pixelify_Sans({
   weight: ['400', '700'],
 })
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "BadgerBash",
   description: "Play quick games with your friends!",
