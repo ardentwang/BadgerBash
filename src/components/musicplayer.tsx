f757f950199ae5032f34239bb88c79b9e51b6082
Arden Wang <ardentwang@gmail.com>
2025-02-13 15:12:54 -0600

    adjusted menu button sizes, moved music player to the right of the screen

--- src/components/musicplayer.tsx
+++ src/components/musicplayer.tsx
@@ -7,7 +7,7 @@ import { Music } from "lucide-react"
 const MusicPlayer = () => {
   return(
     <div>
-        <Button className='absolute mt-5 ml-5 z-50' variant="outline" size="icon">
+        <Button className='absolute right-0 mt-5 mr-5 z-50' variant="outline" size="icon">
             <Music />
         </Button>
     </div>
