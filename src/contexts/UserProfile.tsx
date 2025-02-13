29597167a6686cf2ca4816eafc3e4aef228fc5e8
Arden Wang <ardentwang@gmail.com>
2025-02-13 13:14:00 -0600

    i forgor what i updated - ignore

--- src/contexts/UserProfile.tsx
+++ src/contexts/UserProfile.tsx
@@ -9,13 +9,13 @@ interface GuestAccount{
 
 const id = GenerateUserID()
 
-//should check for duplicates
-//ideally could just check database for current length of users + 1 for new id
+//should check for duplicates in da futura
+//ideally could just check database for current length of users + 1 for new id, depends on wut database
 function GenerateUserID() {
     return (Math.random() + 1).toString(36).substring(7)
 }
 
-//Add User account with authentication later
+//add User account with authentication later
 const UserProfile = createContext<GuestAccount>({
     id: id,
     isHost: false,
